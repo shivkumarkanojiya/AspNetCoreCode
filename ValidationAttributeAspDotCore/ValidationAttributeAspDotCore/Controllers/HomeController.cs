7426dd9e815eee2600a44ce4cba47ed01c04d649
shivkumarkanojiya <132676192+shivkumarkanojiya@users.noreply.github.com>
2023-08-09 15:41:51 +0530

    initail commit

---
+++ ValidationAttributeAspDotCore/ValidationAttributeAspDotCore/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using ValidationAttributeAspDotCore.Models;
+
+namespace ValidationAttributeAspDotCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Student std)
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ValidationAttributeAspDotCore/ValidationAttributeAspDotCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ValidationAttributeAspDotCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
